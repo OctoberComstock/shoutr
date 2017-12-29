bc5851f18f75c0852aaec39ce00bb09ba303ed01
OktoberComstock <oktober.comstock@gmail.com>
2017-12-29 15:25:17 -0800

    show shouts in dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
+  resources :shouts, only: [:create]
+
   resources :users, only: [:create] do
     resource :password,
       controller: "clearance/passwords",
