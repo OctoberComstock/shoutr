a02073e74306d86e9736236846740e6ca97c3f90
OktoberComstock <oktober.comstock@gmail.com>
2017-12-29 15:56:10 -0800

    added show actions for users and shouts

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
